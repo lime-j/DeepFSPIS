71137fe456b1528aceec03fb38ef3e9d2ad33cee
Mingjia Li <mingjiali@tju.edu.cn>
2022-12-28 10:38:58 +0800

    Update adjuster.py

--- models/adjuster.py
+++ models/adjuster.py
@@ -118,7 +118,7 @@ class Adjuster(nn.Module):
         actual_lam = max(0.2, lam)
         tmp = self.fuser(torch.cat([state, actual_lam * siamese_state], dim=-3)) 
         mask =  self.to_edge(tmp)
-        if not inference : return mask
+        if not inference : return mask * grad 
         mask_org = mask.clone()
         thresh = 0.05
         mask_org[mask < thresh] = 0
